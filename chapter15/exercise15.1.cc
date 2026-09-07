06e1f2587d57fb984029b0a937845106b46897b0
lucaslingle <lucasdaxlingle@gmail.com>
2026-09-07 09:40:57 -0700

    chapter15

---
+++ chapter15/exercise15.1.cc
@@ -0,0 +1,6 @@
+/*
+What is a virtual member?
+*/
+
+// a virtual member function is a member function of a base class
+// that is expected to be overridden in derived classes.
---
+++ chapter15/exercise15.2.cc
@@ -0,0 +1,8 @@
+/*
+How does the `protected` access specifier differ from `private`?
+*/
+
+// private data members and member functions are visible to other instances 
+// of the same class, but not to other classes.
+
+// protected data members and member functions are visible to derived classes.
